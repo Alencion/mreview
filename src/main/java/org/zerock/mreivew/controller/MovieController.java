c2a9ea593e32fa5ff8d3063aad59aed5ff4c059a
Alencion <rhqnrdl625@gmail.com>
2022-01-13 16:12:14 +0900

    [추가] 영화 등록 페이지 레이아웃 추가

---
+++ src/main/java/org/zerock/mreivew/controller/MovieController.java
@@ -0,0 +1,16 @@
+package org.zerock.mreivew.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movie")
+@Slf4j
+public class MovieController {
+
+    @GetMapping("/register")
+    public void register() {
+    }
+}
