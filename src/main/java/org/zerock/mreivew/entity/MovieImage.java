4ffdfa27c84bb469bcd317fca75537ceab71e241
Alencion <rhqnrdl625@gmail.com>
2022-01-13 09:42:19 +0900

    [추가] 필요한 모든 엔티티 추가
    - Member, Moive, MovieImage, Review 엔티티 추가
    - BaseEntity는 Auditing을 담당하는 공통 필드 포함.
    - Member, Moive, Review만 BaseEntity 상속
    - MreviewApplication에 JPAauditing enable 어노테이션 추가
    - Review 엔티티는 Member와 Movie의 연결 엔티티

---
+++ src/main/java/org/zerock/mreivew/entity/MovieImage.java
@@ -0,0 +1,27 @@
+package org.zerock.mreivew.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString(exclude = "movie")
+public class MovieImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}
