b61e124a52f4d997f336b61165387cddf49bee98
Alencion <rhqnrdl625@gmail.com>
2022-01-13 16:39:07 +0900

    [추가] 영화 업로드 로직 추가 - #3
    - 영화 업로드 service 기능 추가

--- src/main/java/org/zerock/mreivew/controller/MovieController.java
+++ src/main/java/org/zerock/mreivew/controller/MovieController.java
@@ -1,16 +1,35 @@
 package org.zerock.mreivew.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.mreivew.dto.MovieDTO;
+import org.zerock.mreivew.service.MovieService;
 
 @Controller
 @RequestMapping("/movie")
 @Slf4j
 public class MovieController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping("/register")
     public void register() {
+    }
+
+    @PostMapping("/register")
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
+        log.info("movieDTO: {}", movieDTO);
+
+        Long mno = this.movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
     }
 }
---
+++ src/main/java/org/zerock/mreivew/service/MovieServiceImpl.java
@@ -0,0 +1,37 @@
+package org.zerock.mreivew.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mreivew.dto.MovieDTO;
+import org.zerock.mreivew.entity.Movie;
+import org.zerock.mreivew.entity.MovieImage;
+import org.zerock.mreivew.repository.MovieImageRepository;
+import org.zerock.mreivew.repository.MovieRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+    private final MovieImageRepository movieImageRepository;
+
+    @Transactional
+    @Override
+    public Long register(MovieDTO movieDTO) {
+
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+        this.movieRepository.save(movie);
+
+        movieImageList.forEach(this.movieImageRepository::save);
+
+
+        return movie.getMno();
+    }
+}
