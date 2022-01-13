dffa7c6ca471ac720aaa15789bc21a95161d8737
Alencion <rhqnrdl625@gmail.com>
2022-01-13 09:30:13 +0900

    [설정] 프로젝트 초기 설정
    - static 파일들 추가
    - application.properties 설정 추가 (db관련)
    - .gitignore 설정추가
    - build.gradle에 타임리프 시간 모듈, mariadb 모듈 추가

---
+++ src/main/java/org/zerock/mreivew/MreivewApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mreivew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreivewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MreivewApplication.class, args);
+	}
+
+}
