f5bdb46b0550b95cd27af6c3eb9ea1e919b7bf64
Alencion <rhqnrdl625@gmail.com>
2022-01-13 12:10:47 +0900

    [추가] 영화에 대한 리뷰 조회 findByMoive 추가
    - 테스트 코드 추가

--- src/main/java/org/zerock/mreivew/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreivew/repository/ReviewRepository.java
@@ -1,7 +1,14 @@
 package org.zerock.mreivew.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.mreivew.entity.Movie;
 import org.zerock.mreivew.entity.Review;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }
