eedd9bb2a86ae2ea23164a5391b65676d494253a
Alencion <rhqnrdl625@gmail.com>
2022-01-14 09:58:56 +0900

    [추가] 특정 영화 조회 기능 구현 - #5

--- src/main/java/org/zerock/mreivew/service/MovieServiceImpl.java
+++ src/main/java/org/zerock/mreivew/service/MovieServiceImpl.java
@@ -14,6 +14,7 @@ import org.zerock.mreivew.entity.MovieImage;
 import org.zerock.mreivew.repository.MovieImageRepository;
 import org.zerock.mreivew.repository.MovieRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -54,5 +55,25 @@ public class MovieServiceImpl implements MovieService {
         );
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public MovieDTO getMovie(Long mno) {
+
+        List<Object[]> result = this.movieRepository.getMovieWithAll(mno);
+
+        Movie movie = (Movie) result.get(0)[0];
+
+        List<MovieImage> movieImageList = new ArrayList<>();
+
+        result.forEach(arr -> {
+            MovieImage movieImage = (MovieImage) arr[1];
+            movieImageList.add(movieImage);
+        });
+
+        Double avg = (Double) result.get(0)[2];
+        Long reviewCnt = (Long) result.get(0)[3];
+
+        return entitiesToDTO(movie, movieImageList, avg, reviewCnt);
     }
 }
